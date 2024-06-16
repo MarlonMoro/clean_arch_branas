fd7de130bbdd67ec7644c75192bfc6dc3fab5195
Marlon Moro <marlon.moro@gmail.com>
2024-06-16 17:52:41 -0300

    refactor: separando as camadas e dependencias

--- src/application.ts
+++ src/application.ts
@@ -1,38 +1,47 @@
 import crypto from "crypto";
 import { validate } from "./validateCpf";
-import { getAccountByEmail, getAccountById, saveAccount } from "./resource";
+import { AccountDAO } from "./resource";
 
-export async function signup(input: any): Promise<any> {
-  const account = input;
-  account.id = crypto.randomUUID();
-  const existingAccount = await getAccountByEmail(input.email);
-  if (existingAccount) throw new Error("Account already exists");
-  if (!account.name.match(/[a-zA-Z] [a-zA-Z]+/)) throw new Error("Invalid Name");
-  if (!account.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
-  if (!validate(account.cpf)) throw new Error("Invalid cpf");
-  if (
-    !account.isDriver &&
-    account.carPlate &&
-    !account.carPlate.match(/[A-Z]{3}[0-9]{4}/)
-  )
-    return -5;
-  await saveAccount(account);
-  return {
-    accountId: account.id,
-  };
+export class Signup {
+  constructor(readonly accountDAO: AccountDAO) {}
+
+  async execute(input: any): Promise<any> {
+    const account = input;
+    account.id = crypto.randomUUID();
+    const existingAccount = await this.accountDAO.getAccountByEmail(account.email);
+    if (existingAccount) throw new Error("Account already exists");
+    if (!account.name.match(/[a-zA-Z] [a-zA-Z]+/))
+      throw new Error("Invalid Name");
+    if (!account.email.match(/^(.+)@(.+)$/)) throw new Error("Invalid email");
+    if (!validate(account.cpf)) throw new Error("Invalid cpf");
+    if (
+      !account.isDriver &&
+      account.carPlate &&
+      !account.carPlate.match(/[A-Z]{3}[0-9]{4}/)
+    )
+      throw new Error("Invalid carPlate");
+    await this.accountDAO.saveAccount(account);
+    return {
+      accountId: account.id,
+    };
+  }
 }
 
-export async function getAccount(accountIdentifier: string) {
-  const acc = await getAccountById(accountIdentifier)
-    if (!acc) {
-      return;
-    }
-    const {
-      account_id: accountId,
-      is_passenger: isPassenger,
-      is_driver: isDriver,
-      car_plate: carPlate,
-      ...otherProperties
-    } = acc;
-    return { accountId, isPassenger, isDriver, carPlate, ...otherProperties };
+export class GetAccount {
+  constructor(readonly accountDAO: AccountDAO) {}
+
+  async execute(accountIdentifier: string) {
+    const acc = await this.accountDAO.getAccountById(accountIdentifier);
+  if (!acc) {
+    return;
+  }
+  const {
+    account_id: accountId,
+    is_passenger: isPassenger,
+    is_driver: isDriver,
+    car_plate: carPlate,
+    ...otherProperties
+  } = acc;
+  return { accountId, isPassenger, isDriver, carPlate, ...otherProperties };
+  }
 }
--- src/resource.ts
+++ src/resource.ts
@@ -1,35 +1,45 @@
-import express from "express";
 import pgp from "pg-promise";
-const app = express();
-app.use(express.json());
 
-export async function getAccountByEmail(email: string) {
-  const connection = pgp()("postgres://api:password@localhost:5432/database");
-  const [acc] = await connection.query("select * from cccat16.account where email = $1", [email]);
-  await connection.$pool.end();
-  return acc;
+
+export interface AccountDAO {
+  getAccountByEmail(email: string): Promise<any>;
+  getAccountById(accountIdentifier: string): Promise<any>;
+  saveAccount(account: any): Promise<void>;
 }
 
-export async function getAccountById(accountId: string) {
-  const connection = pgp()("postgres://api:password@localhost:5432/database");
-  const [acc] = await connection.query("select * from cccat16.account where account_id = $1", [accountId]);
-  await connection.$pool.end();
-  return acc;
+export class AccountDAODatabase implements AccountDAO {
+
+  async getAccountByEmail(email: string) {
+    const connection = pgp()("postgres://api:password@localhost:5432/database");
+    const [acc] = await connection.query("select * from cccat16.account where email = $1", [email]);
+    await connection.$pool.end();
+    return acc;
+  }
+  
+  async getAccountById(accountId: string) {
+    const connection = pgp()("postgres://api:password@localhost:5432/database");
+    const [acc] = await connection.query("select * from cccat16.account where account_id = $1", [accountId]);
+    await connection.$pool.end();
+    return acc;
+  }
+  
+  async saveAccount(account: any) {
+    const connection = pgp()("postgres://api:password@localhost:5432/database");
+    await connection.query(
+      "insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
+      [
+        account.id,
+        account.name,
+        account.email,
+        account.cpf,
+        account.carPlate,
+        !!account.isPassenger,
+        !!account.isDriver,
+      ]
+    );
+    await connection.$pool.end();
+  }
+
 }
 
-export async function saveAccount(account: any) {
-  const connection = pgp()("postgres://api:password@localhost:5432/database");
-  await connection.query(
-    "insert into cccat16.account (account_id, name, email, cpf, car_plate, is_passenger, is_driver) values ($1, $2, $3, $4, $5, $6, $7)",
-    [
-      account.id,
-      account.name,
-      account.email,
-      account.cpf,
-      account.carPlate,
-      !!account.isPassenger,
-      !!account.isDriver,
-    ]
-  );
-  await connection.$pool.end();
-}
+
